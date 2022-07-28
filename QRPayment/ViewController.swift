9ccce7bdcb083f420601120e2247637ee967e32c
erdox <erdogantrpc@gmail.com>
2022-07-28 20:07:58 +0300

    Initial Commit

---
+++ QRPayment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QRPayment
+//
+//  Created by Erdoğan Turpcu on 28.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
